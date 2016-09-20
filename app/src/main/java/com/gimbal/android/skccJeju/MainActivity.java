bbd733b57bf9a44e13f66b66e09f8c27f32e51d0
hyubs <hyub0216@gmail.com>
2016-09-20 17:48:46 +0900

    화면

--- app/src/main/java/com/gimbal/android/skccJeju/MainActivity.java
+++ app/src/main/java/com/gimbal/android/skccJeju/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.ImageButton;
@@ -17,39 +18,40 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("on Create MainActivity");
+        Log.v("onCreate","MainActivity");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_app_main);
 
-        Gimbal.setApiKey(this.getApplication(), "8cbce2c7-6e27-419c-9be1-7b01f1d990e4");
-        startService(new Intent(this, AppService.class));
+//        startService(new Intent(this, AppService.class));
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
 
-        GimbalDAO.setOptInShown(getApplicationContext());
-        enablePlaceMonitoring();
-        enableCommunications();
+//        GimbalDAO.setOptInShown(getApplicationContext());
+//        enablePlaceMonitoring();
+//        enableCommunications();
 
-
-        final ImageButton imgbtn1 = (ImageButton) findViewById(R.id.mainImg1);
-        final ImageButton imgbtn2 = (ImageButton) findViewById(R.id.mainImg2);
-
-        imgbtn1.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                if(event.getAction()== MotionEvent.ACTION_BUTTON_PRESS){
-                    imgbtn1.setVisibility(View.INVISIBLE);
-                    imgbtn2.setVisibility(View.VISIBLE);
-                }else if(event.getAction()== MotionEvent.ACTION_BUTTON_PRESS){
-                    imgbtn1.setVisibility(View.VISIBLE);
-                    imgbtn2.setVisibility(View.INVISIBLE);
-                }
-                return false;
-            }
-        });
+//
+//        final ImageButton imgbtn1 = (ImageButton) findViewById(R.id.mainImg1);
+//        final ImageButton imgbtn2 = (ImageButton) findViewById(R.id.mainImg2);
+//
+//        imgbtn1.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                if (event.getAction() == MotionEvent.ACTION_BUTTON_PRESS) {
+//                    imgbtn1.setVisibility(View.INVISIBLE);
+//                    imgbtn2.setVisibility(View.VISIBLE);
+//                } else if (event.getAction() == MotionEvent.ACTION_BUTTON_PRESS) {
+//                    imgbtn1.setVisibility(View.VISIBLE);
+//                    imgbtn2.setVisibility(View.INVISIBLE);
+//                }
+//                return false;
+//            }
+//        });
     }
 
-    private void enablePlaceMonitoring(){
+    private void enablePlaceMonitoring() {
         permissions = new LocationPermissions(this);
         permissions.checkAndRequestPermission();
     }
@@ -59,18 +61,25 @@ public class MainActivity extends AppCompatActivity {
         PushRegistrationHelper.registerForPush();
     }
 
-    public void onBeaconConHisClick(View view){
+    public void onBeaconConHisClick(View view) {
         Toast.makeText(getApplicationContext(), "비콘 접속이력 .",
                 Toast.LENGTH_SHORT).show();
-        Intent  intent = new Intent(this, AppActivity.class);
-        startActivity(intent);
-    }
-    public void onStartBtnClick(View view){
-        Toast.makeText(getApplicationContext(), "제주동문시장 관광 도우미.",
-                Toast.LENGTH_SHORT).show();
-        Intent  intent = new Intent(this, BeaconMapActivity.class);
+        Intent intent = new Intent(this, AppActivity.class);
         startActivity(intent);
     }
 
+    public void onStartBtnClick(View view) {
+        Toast.makeText(getApplicationContext(), "BT SERVICE? Start.",
+                Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, BeaconMapActivity.class);
+        startActivity(intent);
+    }
+
+    public void onAppStartBtnClick(View view) {
+        Toast.makeText(getApplicationContext(), "시장하쥬? Start",
+                Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, AppMenuActivity.class);
+        startActivity(intent);
+    }
 
 }
