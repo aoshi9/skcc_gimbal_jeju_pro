d25776fbfeb57d38ec6fe1910177ebe18c45aaaf
hyubs <hyub0216@gmail.com>
2016-09-21 21:50:51 +0900

    화면

---
+++ app/src/main/java/com/gimbal/android/skccJeju/Splash.java
@@ -0,0 +1,32 @@
+package com.gimbal.android.skccJeju;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import com.gimbal.android.CommunicationManager;
+import com.gimbal.android.Gimbal;
+
+public class Splash extends AppCompatActivity {
+    LocationPermissions permissions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash);
+
+        Handler hd = new Handler();
+        hd.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                finish();       // 3 초후 이미지를 닫아버림
+            }
+        }, 3000);
+    }
+
+}
