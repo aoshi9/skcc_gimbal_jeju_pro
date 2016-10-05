101658be7efe61503d1fc30bc4750dfb931ad9dc
hyubs <hyub0216@gmail.com>
2016-10-05 16:40:41 +0900

    비콘맵

--- app/src/main/java/com/gimbal/android/skccJeju/BeaconMapActivity.java
+++ app/src/main/java/com/gimbal/android/skccJeju/BeaconMapActivity.java
@@ -150,18 +150,15 @@ public class BeaconMapActivity extends AppCompatActivity implements MapView.POII
             public void onItemClick(AdapterView<?> parent, final View view, int position, long id)
             {
                 // 선택된 item이 click 되어 있는 상태인지 체크하는 부분 (최준형)
-                Log.v("HoyoungLog  :  ", "isEnabled : " + parent.getChildAt(position).isEnabled());
-                if(parent.getChildAt(position).isEnabled()) {
+                if(parent.getChildAt(position).isEnabled()  ) {
 //                    view.setBackgroundColor(Color.GRAY);             // 선택했을 때, 회색으로 하이라이트
 
                     Toast.makeText(getApplicationContext(), list.get(position).get(SIXTH_COLUMN) + " 장바구니 담기", Toast.LENGTH_SHORT).show();
 
                     parent.getChildAt(position).setEnabled(false);
-                    Log.v("tempLog  :  ", "basketYShopItemSelect : " + list.get(position).get(FIRST_COLUMN) + ", " + list.get(position).get(EIGHTH_COLUMN));
                     dbHelper.SotBeaconInfoItemBasketY(list.get(position).get(FIRST_COLUMN), list.get(position).get(EIGHTH_COLUMN));     // DB BASKET_YN 컬럼 update
-                    Log.v("tempLog  :  ", "basketYShopItemSelect : " + dbHelper.basketYShopItemSelect());
                     TextView tv = (TextView) view.findViewById(R.id.basketYn);
-                    tv.setText("V");
+                    tv.setText("       V");
                     tv.setTextColor(Color.parseColor("#EC6652"));
                     tv.setTypeface(Typeface.DEFAULT_BOLD);
                 } else {
