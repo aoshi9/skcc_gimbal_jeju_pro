960c9f108d06328d748ddc2d284d61b5e500996f
hyubs <hyub0216@gmail.com>
2016-10-05 11:02:12 +0900

    detail 화면 java 변경 이상협

--- app/src/main/java/com/gimbal/android/skccJeju/ItemList2Adapter.java
+++ app/src/main/java/com/gimbal/android/skccJeju/ItemList2Adapter.java
@@ -18,6 +18,9 @@ package com.gimbal.android.skccJeju;
 
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -39,17 +42,15 @@ public class ItemList2Adapter extends BaseAdapter {
 
     public ArrayList<HashMap<String, String>> list;
     private Activity activity;
-    TextView itemName;
-    TextView itemNo;
-    TextView itemPrice;
-    TextView bcNo;
-    TextView itemDesc;
-    TextView itemEvent;
+    private Context ctx;
+    private Typeface tf;
 
-    public ItemList2Adapter(Activity activity, ArrayList<HashMap<String, String>> list) {
+    public ItemList2Adapter(Activity activity, ArrayList<HashMap<String, String>> list,Context ctx) {
         super();
         this.activity = activity;
         this.list = list;
+        this.ctx = ctx;
+        this.tf = Typeface.createFromAsset(this.ctx.getAssets(),"BMJUA_ttf.ttf");
     }
 
     @Override
@@ -63,27 +64,56 @@ public class ItemList2Adapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        LayoutInflater inflater = activity.getLayoutInflater();
 
+        ViewHolder holder;
         if(convertView == null) {
+            LayoutInflater inflater = activity.getLayoutInflater();
             convertView = inflater.inflate(R.layout.list_item2, null);
 
-            bcNo = (TextView) convertView.findViewById((R.id.bc_no));
-            itemNo = (TextView) convertView.findViewById(R.id.itemNo);
-            itemName = (TextView) convertView.findViewById(R.id.itemName);
-            itemPrice = (TextView) convertView.findViewById(R.id.itemPrice);
-            itemDesc = (TextView) convertView.findViewById(R.id.itemDesc);
-            itemEvent = (TextView) convertView.findViewById(R.id.itemEvent);
+            holder = new ViewHolder();
+            holder.bcNo = (TextView) convertView.findViewById((R.id.bc_no));
+            holder.itemNo = (TextView) convertView.findViewById(R.id.itemNo);
+            holder.itemName = (TextView) convertView.findViewById(R.id.itemName);
+            holder.itemPrice = (TextView) convertView.findViewById(R.id.itemPrice);
+            holder.itemDesc = (TextView) convertView.findViewById(R.id.itemDesc);
+            holder.itemEvent = (TextView) convertView.findViewById(R.id.itemEvent);
+
+            convertView.setTag(holder);
+        }else {
+            holder = (ViewHolder) convertView.getTag();
         }
 
         HashMap<String, String> map = list.get(position);
-        bcNo.setText(map.get(FIRST_COLUMN));
-        itemNo.setText(map.get(SECOND_COLUMN));
-        itemName.setText(map.get(THIRD_COLUMN));
-        itemPrice.setText(map.get(FOURTH_COLUMN));
-        itemDesc.setText(map.get(FIFTH_COLUMN));
-        itemEvent.setText(map.get(SIXTH_COLUMN));
+        holder.bcNo.setText(map.get(FIRST_COLUMN));
+        holder.itemNo.setText(map.get(SECOND_COLUMN));
+        holder.itemName.setText(map.get(THIRD_COLUMN));
+        holder.itemPrice.setText(map.get(FOURTH_COLUMN));
+        holder.itemDesc.setText(map.get(FIFTH_COLUMN));
+        holder.itemEvent.setText(map.get(SIXTH_COLUMN));
+
+        holder.bcNo.setTypeface(tf);
+        holder.itemNo.setTypeface(tf);
+        holder.itemName.setTypeface(tf);
+        holder.itemPrice.setTypeface(tf);
+        holder.itemDesc.setTypeface(tf);
+        holder.itemEvent.setTypeface(tf);
+
+        holder.bcNo.setTextColor(Color.parseColor("#000000"));
+        holder.itemNo.setTextColor(Color.parseColor("#000000"));
+        holder.itemName.setTextColor(Color.parseColor("#000000"));
+        holder.itemPrice.setTextColor(Color.parseColor("#000000"));
+        holder.itemDesc.setTextColor(Color.parseColor("#000000"));
+        holder.itemEvent.setTextColor(Color.parseColor("#000000"));
 
         return convertView;
+    }
+
+    private class ViewHolder {
+        TextView itemName;
+        TextView itemNo;
+        TextView itemPrice;
+        TextView bcNo;
+        TextView itemDesc;
+        TextView itemEvent;
     }
 }
